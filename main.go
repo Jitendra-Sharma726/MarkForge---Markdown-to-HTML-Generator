a6d2af3420a904bd9ed7cbcf1840fa137bdd1859
Jitendra Kumar Sharma <136931188+Jitendra-Sharma726@users.noreply.github.com>
2026-08-20 23:16:44 +0530

    Update main.go

--- main.go
+++ main.go
@@ -14,4 +14,79 @@ func processMarkdown(scanner *bufio.Scanner, file *os.File) {
 
   for scanner.scan()
   //Clean up spacing and sanitize input to prevent XSS attacks
-  rawText := strings.Trim
+  rawText := strings.TrimSpace(scanner.Text())
+  line := html.EscapeString(rawText)
+
+  // Skip empty lines and close open lists
+  if line == "" {
+    if inList {
+        file.WriteString("</ul>\n")
+        inList = false
+      }
+    continue
+  }
+
+  //Parse heading (H6 down to H1 to avoid false matches)
+  if strings.HasPrefix(line, "###### ") {
+    content := strings.TrimPrefix(line, "###### ")
+    file.WriteString("<h6>" + parseInLine(content) + "</h6>\n")
+    continue
+    }
+  if strings.HasPrefix(line, "##### ") {
+    content := strings.TrimPrefix(line, "##### ")
+    file.WriteString("<h5" + parseInline(content) + "</h5>\n")
+    continue
+  }
+  if strings.HasPrefix(line, "#### ") {
+    content := strings.TrimPrefix(line, "#### ")
+    file.WriteString("<h4>" + parseInline(content) + "</h4>\n")
+    continue
+  }
+  if strings.HasPrefix(line, "### ") {
+    content := strings.TrimPrefix(line, "### ")
+    file.WriteString("<h3>" + parseInline(content) + "</h3>\n")
+    continue
+  }
+  if strings.HasPrefix(line, "## "){
+    content := strings.TrimPrefix(line, "## ")
+    file.WriteString("<h2>" + parseInline(content) + "</h2>\n")
+    continue
+  }
+  if strings.HasPrefix(line, "# ") {
+    content := strings.TrimPrefix(line, "# ")
+    file.WriteString("<h1>" + parseInline(content) + "</h1>\n")
+    continue
+  }
+
+  //Handle list items
+
+
+
+
+
+
+
+
+                                          
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                  
